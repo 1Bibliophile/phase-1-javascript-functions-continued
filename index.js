bbd1beabfb6ef0f615f1c6ad15f8b73058f4a023
David Morgenstern <DavidtheConfident@gmail.com>
2021-09-30 16:34:55 -0500

    commit tree 6d07f9b65cf1d0b26dceb3ac0db6f050c870122c

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+function saturdayFun(fun = 'roller-skate'){
+    console.log(`This Saturday, I want to ${fun}!`)
+}
+
+function mondayWork(work = "go to the office"){
+    console.log(`This Monday, I will ${work}.`)
+}
+
+function result(a){
+    wrapAdjective("||", a)
+
+    let b = `You are ${a}!`
+    return b
+}
+
+function wrapAdjective(symbol, sentence){
+    /*sentence.unshift(symbol)
+    sentence.push(symbol)
+    */
+
+    sentence = `${symbol}${sentence}${symbol}`
+
+    return symbol, sentence;
+}
+
+let emphatic = result("a dedicated programmer")
+
+//mondayWork("work from home")
